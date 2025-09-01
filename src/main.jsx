2ad5a18085948bfe523ee60386e36e5250037ab3
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-09-02 06:45:48 +0900

    chore: Pretendard 폰트 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App.jsx';
+import './index.css';
+import './assets/fonts/pretendard-subset.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
