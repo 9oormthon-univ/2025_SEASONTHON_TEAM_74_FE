6c7236051107386a82326059eed8c0cdda09fcd0
san-sae <choi.yoo.lim.12@gmail.com>
2025-09-06 22:15:07 +0900

    [FIX] 로그인 미상태 경우, 방 생성와 방 참여 방지

--- src/components/PageButton.jsx
+++ src/components/PageButton.jsx
@@ -1,9 +1,23 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../context/authStore";
+
 import styled from "styled-components";
 
 const PageButton = ({ to, icon, alt, children }) => {
+  const navigate = useNavigate();
+
+  const { user } = useAuthStore();
+
+  const navigateToPage = () => {
+    if(!user) {
+      alert("로그인을 먼저 진행해주세요!");
+      return;
+    }
+    navigate(to);
+  };
+
   return (
-    <Wrapper to={to}>
+    <Wrapper onClick={navigateToPage}>
       <img src={icon} alt={alt || "아이콘"} />
       <span>{children}</span>
     </Wrapper>
@@ -12,7 +26,7 @@ const PageButton = ({ to, icon, alt, children }) => {
 
 export default PageButton;
 
-const Wrapper = styled(Link)`
+const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
